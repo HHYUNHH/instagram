58f7e2ab351f9fd1514b5cb6e8d5fc3401418dd2
HHYUNHH <112064615+HHYUNHH@users.noreply.github.com>
2023-02-17 22:19:43 +0900

    Add files via upload

--- instagram_bot.py
+++ instagram_bot.py
@@ -3,6 +3,23 @@ from pheed import pheed
 from highlight import highlight
 
 def instagram_bot(target_list:list, ID, PW, mode=1, view='off'):
+    '''
+    Args:
+        target_list (list): 인스타그램 계정
+        
+        ID, PW (str): 로그인 할 인스타그램 계정
+        
+        mode (int): 원하는 모드 설정
+            1: Pheed + Highlight
+            2: Pheed
+            3: Highlight
+            Default: 1
+            
+        view (str): 시각화 설정 (대소문자 구별X)
+            'On': On
+            'Off': Off
+            Default: 'Off'
+    '''
     if not mode in [1, 2, 3]:
         return
     driver = Common.web(view)
--- main.py
+++ main.py
@@ -1,31 +1,9 @@
 from instagram_bot import instagram_bot
 
-'''
-Args:
-    target_list (list):  인스타그램 계정
-    
-    ID, PW (str): 로그인 할 인스타그램 계정
-    
-    mode (int): 원하는 모드 설정
-        1: Pheed + Highlight
-        2: Pheed
-        3: Highlight
-        Default: 1
-        
-    view (str): 시각화 설정 (대소문자 구별X)
-        'On': On
-        'Off': Off
-        Default: 'Off'
-'''
-
 target_list = None
 ID = None
 PW = None
+mode = 1
+view = 'on'
 
-instagram_bot(
-    target_list=target_list,
-    ID=ID,
-    PW=PW,
-    mode=1,
-    view='on'
-    )
+instagram_bot(target_list, ID, PW, mode, view)
